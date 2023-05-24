148cee30fa617feb87b72ba28181e29fd95dfab1
LeandroJunges <leandrojunges21@gmail.com>
2023-05-24 10:49:19 -0300

    :sparkles: content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake  = [
+  {
+    "id":"1",
+    "photo": "https://img-0.journaldunet.com/8Lx3YQeOcn4AzbwIH3NzsuWBc38=/1500x/smart/1502e1bca6ce409c87a54d0da9158267/ccmcms-jdn/16511568.png",
+    "title": "Nova atualização do Angular",
+    "description": "Uma nova versão do angular que promete revolucionar todas as versões anteriores !"
+  },
+  {
+    "id":"2",
+    "photo": "https://img-b.udemycdn.com/course/750x422/4471116_dcf0.jpg",
+    "title": "Saiu a nova versão do Angular",
+    "description": "Uma nova versão do angular que promete revolucionar todas as versões anteriores !"
+  },
+  {
+    "id":"3",
+    "photo": "https://i.ytimg.com/vi/B3FotB9495k/maxresdefault.jpg",
+    "title": "Novas features nesse framework incrivel",
+    "description": "Uma nova versão do angular que promete revolucionar todas as versões anteriores !"
+  },
+  {
+    "id":"4",
+    "photo": "https://www.freecodecamp.org/portuguese/news/content/images/2022/04/react-vs-angular.png",
+    "title": "Veja a diferença de Angular para React",
+    "description": "Uma nova versão do angular que promete revolucionar todas as versões anteriores !"
+  },
+];
